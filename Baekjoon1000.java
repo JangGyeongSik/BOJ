68a2fdd01ed6f7d6ab2657cf86e32d09b7d15d16
Gyeong-sik, Jang <kqkdn1@naver.com>
2018-11-04 16:33:25 +0900

    Create Baekjoon1000.java

---
+++ Baekjoon1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		b=scan.nextInt();
+		System.out.println(a+b);
+		
+	}
+
+}
