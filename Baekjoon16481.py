a4f1a7f052e8f32c652c126c658a5a4254895042
Gyeong-sik, Jang <kqkdn1@naver.com>
2018-11-18 15:11:20 +0900

    Update Baekjoon16481.py

--- Baekjoon16481.py
+++ Baekjoon16481.py
@@ -1,2 +1,5 @@
+#원 전문가 진우  16481
+#평면에 있는 삼각형 ABC의 서로 다른 위치에 있는 세 방접원의 반지름의 길이가 r1, r2, r3일 때, 삼각형 ABC의 내접원의 반지름을 구하시오.
+#Baekjoon/user/kqkdn
 a, b, c= map(int, input().split())
 print(1/(1/a+1/b+1/c))
